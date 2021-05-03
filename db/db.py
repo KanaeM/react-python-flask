daaba72264cf1cb0598f638fff37d19159927192
KanaeM <elsa.josephs@gmail.com>
2021-05-02 18:45:15 -0700

    Initial flask sesetup

---
+++ db/db.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+  return 'Hello, this is the Flask setup test'
+
+if __name__ == "__main__":
+  app.run(debug=True)
+  
